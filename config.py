629afc97e6a3adaf19555bdde8ee9cbead1a051b
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-06-27 16:19:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from urllib.parse import urlparse
 redis_url = os.getenv('REDIS_URL', 'localhost')
 parsed_url = urlparse(redis_url)
 
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-REDIS_PORT = os.getenv('REDIS_PORT', 6379)
-REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
+REDIS_HOST = os.getenv('REDISHOST', 'localhost')
+REDIS_PORT = os.getenv('REDISPORT', 6379)
+REDIS_PASSWORD = os.getenv('REDISPASSWORD', None)
